7e9059c01dd8067f331e357d0c98a7da904756c8
Eric Tunn <erictunn@gmail.com>
2026-07-26 13:35:12 +0100

    Updated public API

--- src/ne_loader/__init__.py
+++ src/ne_loader/__init__.py
@@ -1,9 +1,9 @@
 """Natural Earth Loader package.
 
-See map_loader.py for public API entrypoint.
+See data_loader.py and map_loader.py for public API entrypoints.
 """
 
-from .data_loader import download_dataset
+from .data_loader import download_dataset, fetch_dataset
 from .map_loader import Resolution, get_natural_earth
 
-__all__ = ["Resolution", "download_dataset", "get_natural_earth"]
+__all__ = ["Resolution", "download_dataset", "fetch_dataset", "get_natural_earth"]
