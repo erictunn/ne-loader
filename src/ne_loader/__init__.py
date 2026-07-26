141a5060437463458070c5758b4a3c8bb2f72b41
Eric Tunn <erictunn@gmail.com>
2026-07-26 14:46:21 +0100

    CHORE: Updated public API

--- src/ne_loader/__init__.py
+++ src/ne_loader/__init__.py
@@ -3,7 +3,13 @@
 See data_loader.py and map_loader.py for public API entrypoints.
 """
 
-from .data_loader import download_dataset, fetch_dataset
+from .data_loader import download_dataset, fetch_dataset, get_dataset
 from .map_loader import Resolution, get_natural_earth
 
-__all__ = ["Resolution", "download_dataset", "fetch_dataset", "get_natural_earth"]
+__all__ = [
+    "Resolution",
+    "download_dataset",
+    "fetch_dataset",
+    "get_dataset",
+    "get_natural_earth",
+]
